0678117e06feb808c964b7d511272d6da697d70a
o-meraba <omeraba93@gmail.com>
2021-01-17 02:17:23 +0300

    Updated multiplication-program

--- Mini-Projeler/multiplication-program.py
+++ Mini-Projeler/multiplication-program.py
@@ -3,11 +3,11 @@
 #There are four level for random multiply questions
 #You can choose your level from 1 to 4 /Level1-->(1-25) Level2-->(25-50) Level3-->(50-75) Level4-->(75-100)
 
-import random
+import random # To import random module
 
 
-
-def generate_randoms(option):
+# To generate randoms with entered option and return result multiply of two number
+def generate_randoms(option): 
    if(option == 1):
     a = random.randint(1, 25)
     b = random.randint(1, 25)
@@ -24,7 +24,7 @@ def generate_randoms(option):
      a = random.randint(75, 100)
      b = random.randint(75, 100)
    
-   carp=lambda x,y : x*y 
+   carp=lambda x,y : x*y # To multiply two number using LAMBDA
    print("{} x {} : ".format(a,b))
    
    return carp(a,b)
@@ -33,28 +33,52 @@ def generate_randoms(option):
 
 def main():
 
-  while(1):  # For infinite loop
+ 
+ while(1):  # For infinite loop
 
-   # Print options
-   print("""1) Basic (1 - 25) 
-          \n2) Beginner (25 - 50)
-          \n3) Intermadiate (50 - 75)
-          \n4) Advance (75 - 100) 
-         """)
+   try: # To catch errors
+     # Print options
+     print("""1) Basic (1 - 25) 
+            \n2) Beginner (25 - 50)
+            \n3) Intermadiate (50 - 75)
+            \n4) Advance (75 - 100) 
+           """)
    
-   option = int(input("Please enter your option (9 for quit):"))  # Enter Option
+     option = int(input("Please enter your option (9 for quit):"))  # Enter Option
 
-   if(option == 9): # To check option for quit from program
-      print("Good Bye, See you soon again..")
-      break
-   else:      
-      result=generate_randoms(option) # To get result from generate_randoms function with entered option
+     if(option == 9): # To check option for quit from program
+        print("\n***Good Bye, See you soon again..***\n")
+        break
+     elif(option == 1 or option == 2 or option == 3 or option == 4):      
+        result=generate_randoms(option) # To get result from generate_randoms function with entered option
+     else:
+       print('***************************')
+       print("WRONG OPTION, please enter your option again..")
+       print('***************************')
+       continue
+     answer=int(input('Enter your answer:\n')) # To request an answer for question
+   
+     if(answer== result):  # To check answer
+       print('***************************')
+       print('      Congrats...')
+       print('***************************')
+    
+     else:
+       print('***************************')
+       print(" Your answer isn't correct.")
+       print(" Correct answer is {}.".format(result))
 
-   answer=int(input('Enter your answer:\n')) # To request an answer for question
-   
-   if(answer== result):  # To check answer
-     print('Congrats...\n')
-   else:
-     print("Your answer isn't correct.\n")
-   
+   except SyntaxError: # To catch SyntaxError
+       print('***************************')
+       print("WRONG INPUT, please enter input again...")     
+       print('***************************')
+       continue      
+
+   except NameError: # To catch NameError 
+       print('***************************')
+       print("WRONG INPUT, please enter input again...")     
+       print('***************************')
+       continue     
+
+
 main() # To execute Main Function
