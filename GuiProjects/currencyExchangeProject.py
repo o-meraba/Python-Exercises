1bf81fa1ea60d63b1c6c896406cb5a16ec6b4548
omeraba <omeraba93@gmail.com>
2022-04-01 02:12:58 +0300

    added refresh code

--- GuiProjects/currencyExchangeProject.py
+++ GuiProjects/currencyExchangeProject.py
@@ -1,50 +1,35 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 import sys
 from PyQt5 import QtWidgets
-
-def request_exchange_datas():  # take exchange datas from doviz.com
-    url = "https://www.doviz.com/"
-    response = requests.get(url)
-    html_content = response.content
-    soup = BeautifulSoup(html_content, "html.parser")
-    exchange_datas = soup.find_all("div", attrs={'class': 'item'})
-
-    name_value = []
-    for i in exchange_datas:
-        name = str(i.find("span",attrs={'class':'name'}).text)
-        value = str(i.find("span", attrs={'class': 'value'}).text)
-        name_value.append((name,value))
-
-    for i in name_value:
-        print("Name:",i[0], "Value:", i[1])
-
-
-request_exchange_datas()
+import os
 
 class WindowClass(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.request_exchange_datas()
         self.ui_init()
 
     def ui_init(self):
         self.dollar_name = QtWidgets.QLabel("Dollar")
-        self.dollar_value = QtWidgets.QLabel("DollarValue")
+        self.dollar_value = QtWidgets.QLabel(self.name_value_dict['DOLAR'])
 
         self.euro_name = QtWidgets.QLabel("Euro")
-        self.euro_value = QtWidgets.QLabel("EuroValue")
+        self.euro_value = QtWidgets.QLabel(self.name_value_dict['EURO'])
 
         self.gold_name = QtWidgets.QLabel("Gold")
-        self.gold_value = QtWidgets.QLabel("GoldValue")
+        self.gold_value = QtWidgets.QLabel(self.name_value_dict['GRAM ALTIN'])
 
         self.silver_name = QtWidgets.QLabel("Silver")
-        self.silver_value = QtWidgets.QLabel("SilverValue")
+        self.silver_value = QtWidgets.QLabel(self.name_value_dict['GÜMÜŞ'])
 
         self.sterlin_name = QtWidgets.QLabel("Sterlin")
-        self.sterlin_value = QtWidgets.QLabel("SterlinValue")
+        self.sterlin_value = QtWidgets.QLabel(self.name_value_dict['STERLİN'])
 
         self.bitcoin_name = QtWidgets.QLabel("Bitcoin")
-        self.bitcoin_value = QtWidgets.QLabel("BitcoinValue")
+        self.bitcoin_value = QtWidgets.QLabel(self.name_value_dict['BITCOIN'])
 
         h_dollar_box = QtWidgets.QHBoxLayout()
         h_dollar_box.addWidget(self.dollar_name)
@@ -70,6 +55,8 @@ class WindowClass(QtWidgets.QWidget):
         h_bitcoin_box.addWidget(self.bitcoin_name)
         h_bitcoin_box.addWidget(self.bitcoin_value)
 
+        self.refresh_button = QtWidgets.QPushButton("Refresh")
+
         v_box = QtWidgets.QVBoxLayout()
         v_box.addLayout(h_gold_box)
         v_box.addLayout(h_dollar_box)
@@ -77,17 +64,37 @@ class WindowClass(QtWidgets.QWidget):
         v_box.addLayout(h_sterlin_box)
         v_box.addLayout(h_bitcoin_box)
         v_box.addLayout(h_silver_box)
-
         v_box.addStretch()
-
+        v_box.addWidget(self.refresh_button)
+        self.refresh_button.clicked.connect(self.refresh_func)
         self.setWindowTitle("Exchange Currency Values")
         self.setGeometry(100,100,300,300)
         self.setLayout(v_box)
         self.show()
 
+    def refresh_func(self):
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+
+    def request_exchange_datas(self):  # take exchange datas from doviz.com
+        self.url = "https://www.doviz.com/"
+        self.response = requests.get(self.url)
+        self.html_content = self.response.content
+        self.soup = BeautifulSoup(self.html_content, "html.parser")
+        self.exchange_datas = self.soup.find_all("div", attrs={'class': 'item'})
+
+        self.name_value_dict = {}
+        for i in self.exchange_datas:
+            self.name = str(i.find("span", attrs={'class': 'name'}).text)
+            self.value = str(i.find("span", attrs={'class': 'value'}).text)
+            self.name_value_dict[self.name] = self.value
+
+
 def windowShow():
     app = QtWidgets.QApplication(sys.argv)
     window = WindowClass()
     sys.exit(app.exec_())
+
+
 
 windowShow()
