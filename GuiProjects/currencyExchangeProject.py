9239b38deab77f37c01b99266d32d9d41cc40e70
omeraba <omeraba93@gmail.com>
2022-04-02 01:22:58 +0300

    added properties to currencyExchangeProject

--- GuiProjects/currencyExchangeProject.py
+++ GuiProjects/currencyExchangeProject.py
@@ -31,6 +31,38 @@ class WindowClass(QtWidgets.QWidget):
         self.bitcoin_name = QtWidgets.QLabel("Bitcoin")
         self.bitcoin_value = QtWidgets.QLabel(self.name_value_dict['BITCOIN'])
 
+        self.amount_label = QtWidgets.QLabel("Amount")
+        self.amount_textLine = QtWidgets.QLineEdit()
+
+        self.from_currency = QtWidgets.QLabel("From Currency")
+        self.from_currency_options = QtWidgets.QComboBox()
+        self.from_currency_options.addItems(["Dollar","Euro","Sterlin","Gold","Silver","Bitcoin","Turkish Lira"])
+
+
+        self.to_currency = QtWidgets.QLabel("To Currency")
+        self.to_currency_options = QtWidgets.QComboBox()
+        self.to_currency_options.addItems(["Turkish Lira","Dollar","Euro","Sterlin","Gold","Silver","Bitcoin"])
+
+        self.convert_button = QtWidgets.QPushButton("Convert")
+        self.result_label = QtWidgets.QLabel("Result")
+        self.result_conv = QtWidgets.QLabel()
+
+        h_result_box = QtWidgets.QHBoxLayout()
+        h_result_box.addWidget(self.result_label)
+        h_result_box.addWidget(self.result_conv)
+
+        h_amount_box = QtWidgets.QHBoxLayout()
+        h_amount_box.addWidget(self.amount_label)
+        h_amount_box.addWidget(self.amount_textLine)
+
+        h_from_currency_opt = QtWidgets.QHBoxLayout()
+        h_from_currency_opt.addWidget(self.from_currency)
+        h_from_currency_opt.addWidget(self.from_currency_options)
+
+        h_to_currency_opt = QtWidgets.QHBoxLayout()
+        h_to_currency_opt.addWidget(self.to_currency)
+        h_to_currency_opt.addWidget(self.to_currency_options)
+
         h_dollar_box = QtWidgets.QHBoxLayout()
         h_dollar_box.addWidget(self.dollar_name)
         h_dollar_box.addWidget(self.dollar_value)
@@ -65,8 +97,15 @@ class WindowClass(QtWidgets.QWidget):
         v_box.addLayout(h_bitcoin_box)
         v_box.addLayout(h_silver_box)
         v_box.addStretch()
+        v_box.addLayout(h_amount_box)
+        v_box.addLayout(h_from_currency_opt)
+        v_box.addLayout(h_to_currency_opt)
+        v_box.addLayout(h_result_box)
+        v_box.addWidget(self.convert_button)
         v_box.addWidget(self.refresh_button)
+
         self.refresh_button.clicked.connect(self.refresh_func)
+        self.convert_button.clicked.connect(self.convert_func)
         self.setWindowTitle("Exchange Currency Values")
         self.setGeometry(100,100,300,300)
         self.setLayout(v_box)
@@ -75,6 +114,13 @@ class WindowClass(QtWidgets.QWidget):
     def refresh_func(self):
         python = sys.executable
         os.execl(python, python, *sys.argv)
+    def convert_func(self):
+        if self.from_currency_options.currentText() == "Dollar" and self.to_currency_options.currentText() == "Turkish Lira" :
+            self.result = int(self.amount_textLine.text()) * float(self.dollar_value.text().replace(',','.'))
+            self.result_conv.setText(str(round(self.result,3)))
+        else:
+            pass
+
 
     def request_exchange_datas(self):  # take exchange datas from doviz.com
         self.url = "https://www.doviz.com/"
